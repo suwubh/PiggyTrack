5bd3730a600c2544d5130873879f119aa9d41fe5
suwubh <subhankarsatpathy69@gmail.com>
2025-08-15 23:16:14 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useMemo, useState } from "react";
 import axios from "axios";
 
-const API_BASE = (process.env.REACT_APP_API_URL || "http://localhost:5000").replace(/\/+$/, ""); // no trailing slash
+const API_BASE = (process.env.REACT_APP_API_URL || "https://piggytrack.onrender.com").replace(/\/+$/, "");
 const GlobalContext = React.createContext();
 
 export const GlobalProvider = ({ children }) => {
