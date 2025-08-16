1f301c2ce36581978184118299abe4a11b03fc56
suwubh <subhankarsatpathy69@gmail.com>
2025-08-16 14:58:38 +0530

    Update Dashboard.js

--- frontend/src/Components/Dashboard/Dashboard.js
+++ frontend/src/Components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 import { useGlobalContext } from '../../context/globalContext';
 import History from '../../History/History';
 import { InnerLayout } from '../../styles/Layouts';
-import { dollar, rupee } from '../../utils/Icons';
+import { rupee } from '../../utils/Icons';
 import Chart from '../Chart/Chart';
 
 function Dashboard() {
