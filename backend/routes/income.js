5b0d7c9e0a50d480543305faeb7b873b67828665
suwubh <subhankarsatpathy69@gmail.com>
2025-08-23 17:15:57 +0530

    Update income.js

--- backend/routes/income.js
+++ backend/routes/income.js
@@ -3,13 +3,10 @@ const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
-// Get incomes
 router.get('/', authMiddleware, getIncomes);
 
-// Add new income
 router.post('/', authMiddleware, addIncome);
 
-// Delete income
 router.delete('/:id', authMiddleware, deleteIncome);
 
 module.exports = router;
